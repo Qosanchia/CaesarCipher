90ce3eb93b5eac59305471ae9e0a9be7fcf9149a
Qosanchia <prickleprickle@gmail.com>
2016-12-05 19:02:41 -0800

    Added Core file - no functionality yet

---
+++ CaesarCipher/crypt.cpp
@@ -0,0 +1,24 @@
+/*
+ * crypt.cpp
+ *
+ *  Created on: Dec 5, 2016
+ *      Author: Qosanchia
+ *
+ *      Core functions for implementing the Caesar Cipher
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+/*
+ * crypt
+ * Takes in a string, outputs a ROT13 version of the string
+ * Should contain all-caps letters only, Later versions will expand this
+ *
+ */
+
+std::string crypt(std::string ins) {
+	std::string outs = ins;
+
+	return outs;
+}
