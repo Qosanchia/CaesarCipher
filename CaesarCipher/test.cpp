bd8e5add6cb0cbe0304f06d48d006c0c5c7ede17
Qosanchia <prickleprickle@gmail.com>
2016-12-07 15:02:20 -0800

    Needs test cases for non-capital strings and arbitrary keys

--- CaesarCipher/test.cpp
+++ CaesarCipher/test.cpp
@@ -10,15 +10,21 @@
 #include <iostream>
 
 std::string coreEncrypt(std::string input);
+std::string coreEncrypt(std::string input, int n);
 
 int main () {
 
+std:: string input = "TEST";
+int key = 10;
+	std::cout << "Test 1, ROT13 default behavior" << "\n";
+	std::cout << "Input: " << input << "\n";
+	std::cout << "Output: " << coreEncrypt(input);
+	std::cout << "\nDecrypted:" << coreEncrypt(coreEncrypt(input));
 
-	std::cout << "Input : Test\n";
-	std::cout << "Output: ";
-	std::cout << coreEncrypt("TEST");
-	std::cout << "\n Decrypted:";
-	std::cout << coreEncrypt(coreEncrypt("TEST"));
+	std::cout << "\nTest 2, arbitrary key" << "\n";
+	std::cout << "Input: " << input << " key = " << key << "\n";
+	std::cout << "Output: " << coreEncrypt(input,key) << "\n";
+	std::cout << "Decryption: " << coreEncrypt(coreEncrypt(input,key), 26-key);
 	return 0;
 }
 
