1bae57934ea257b7166993536b18bfca02835a3b
Qosanchia <prickleprickle@gmail.com>
2016-12-07 15:01:36 -0800

    Added method for arbitrary key

--- CaesarCipher/crypt.cpp
+++ CaesarCipher/crypt.cpp
@@ -9,7 +9,7 @@
 	static std::string LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	/*
- 	 * crypt
+ 	 * coreEncrypt (std::string)
  	 * Takes in a string, outputs a ROT13 version of the string
  	 * Should contain all-caps letters only, Later versions will expand this
  	 *
@@ -22,6 +22,28 @@
 		for (unsigned int i = 0; i < ins.length() ; i++){
 			outs[i] =  LETTERS[(int(ins[i]) + int('A') + 13) % 26];
 		}
+
+		return outs;
+	}
+
+	/*
+	 * std::string coreEncrypt (std::string,int)
+	 * Takes an input string and an input integer, and computes the Caesar
+	 * Cipher of the input string, using the integer as a rotation key
+	 *
+	 * Inputs :
+	 * string - this is expected to be all uppercase letters
+	 * int  - this can be any integer (do negatives work?)
+	 *
+	 * Returns:
+	 * string - The Caesar Cipher of the input string, rotated by int places
+	 */
+	std::string coreEncrypt(std::string ins, int key) {
+		std::string outs = ins;
+
+		for (unsigned int i = 0; i < ins.length() ; i++){
+					outs[i] =  LETTERS[(int(ins[i]) + int('A') + key) % 26];
+				}
 
 		return outs;
 	}
